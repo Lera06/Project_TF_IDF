f9e2de77ac83f40768e5ebf86f8f0b34bc3762ce
Lera06 <lera06@rambler.ru>
2024-04-13 13:53:38 +0300

    Project Update

--- tf_idf/views.py
+++ tf_idf/views.py
@@ -13,23 +13,28 @@ def upload_file(request):
     all_files = FileUpload.objects.all()
 
     # Коллекция загруженных документов:
-    corpus = []
-    # Коллекция без знаков пунктуации:
+    original_corpus = []
+    # Без знаков пунктуации:
     corpus_no_punct = []
-    # Коллекция в нижнем регистре:
+    # В нижнем регистре:
+    corpus_lowercase = []
+    # Без стоп-слов:
     processed_corpus = []
 
     if request.method == 'POST':
         form = UploadForm(request.POST, request.FILES)
 
         for file in request.FILES.getlist('file'):
-            corpus.append(file.read().decode())
+            original_corpus.append(file.read().decode())
 
-        for doc in corpus:
+        for doc in original_corpus:
             corpus_no_punct.append(remove_punctuation(doc))
 
         for doc in corpus_no_punct:
-            processed_corpus.append(get_lowercase(doc))
+            corpus_lowercase.append(get_lowercase(doc))
+
+        for doc in corpus_lowercase:
+            processed_corpus.append(remove_stop_words(doc))
 
         if form.is_valid():
             files = request.FILES.getlist('file')
@@ -42,14 +47,14 @@ def upload_file(request):
         'form': form,
         'all_files': all_files,
         'tf': get_tf(processed_corpus).to_html(),
-        'idf': get_idf(processed_corpus).to_html(),
+        'idf': get_idf(processed_corpus)[1].to_html(),
         'tf_idf': get_tf_idf(processed_corpus).to_html(),
         }
 
     return render(request, 'tf_idf/upload.html', contex)
 
 
-# Вспомогательные методы для предобработки документов:
+# Вспомогательные методы для предварительной обработки документов:
 def remove_punctuation(sentence):
     """Удаляет знаки пунктуации в предложении"""
     formatted_string = ''
@@ -61,6 +66,7 @@ def remove_punctuation(sentence):
 
 
 def get_lowercase(sentence):
+    """Переводит в нижний регистр"""
     formatted_lst = []
     for word in sentence.split(' '):
         formatted_lst.append(word.lower())
@@ -68,14 +74,22 @@ def get_lowercase(sentence):
     return ' '.join(formatted_lst)
 
 
+def remove_stop_words(doc):
+    """Удаляет стоп-слова"""
+    stop_words = get_stop_words('english')
+    filtered_words = []
+
+    for word in doc.split(' '):
+        if word not in stop_words and word != '':
+            filtered_words.append(word)
+
+    return ' '.join(filtered_words)
+
+
 def text_processing(docs):
     """Создает коллекцию из уникальных слов"""
-    # Определим коллекцию уникальных слов в коллекции уже предварительно обработанных документов:
+    # Определим коллекцию уникальных слов в уже предварительно обработанных документах:
     words_set = {word for doc in docs for word in doc.split(' ')}
-
-    # Удаляем стоп-слова и получаем окончательный список уникальных слов:
-    # stop_words = get_stop_words('english')
-    # filtered_unique_words = [word for word in words_set if word not in stop_words]
 
     # Общее число документов в коллекции:
     number_of_docs = len(docs)
@@ -95,12 +109,12 @@ def get_tf(docs):
     # Создаем dataframe с нулевыми значениями: d = pd.DataFrame(np.zeros((N_rows, N_cols)))
     df_tf = pd.DataFrame(np.zeros((number_of_docs, number_unique_words)), columns=list(words_set))
 
-    # Итерируемся по строкам (кол-во наших документов)
     for i in range(number_of_docs):
         words = docs[i].split(' ')
-        # Обращаемся к значениям в каждой ячейке по имени столбца (соответствует уникальному слову из коллекции)
+        # Обращаемся к значениям в каждой ячейке по имени столбца - уникальному слову из коллекции
         for word in words:
-            df_tf[word][i] = df_tf[word][i] + (1 / len(words))
+            value = df_tf.loc[i, [word]] + (1 / len(words))
+            df_tf.loc[i, [word]] = value
 
     return df_tf
 
@@ -112,22 +126,21 @@ def get_idf(docs):
     number_of_docs = text_processing(docs)[1]
     idf = {}
 
-    # Итерируемся по уникальным словам:
     for word in words_set:
         # Количество документов, которые содержат это слово
         count = 0
 
-        # Число итераций соответствует количеству документов:
         for i in range(number_of_docs):
             if word in docs[i].split():
-                # Если данное уникальное слово присутствует в документе, отмечаем этот документ, увеличивая count на 1
+                # Кода данное слово присутствует в документе, отмечаем этот документ, увеличивая count на 1
                 count += 1
 
         idf[word] = np.log10(number_of_docs / count)
 
     df_idf = pd.DataFrame.from_dict([idf])
+    # df_idf = df_idf.T
 
-    return df_idf
+    return idf, df_idf
 
 
 def get_tf_idf(docs):
@@ -136,12 +149,14 @@ def get_tf_idf(docs):
     words_set = text_processing(docs)[0]
     number_of_docs = text_processing(docs)[1]
     df_tf = get_tf(docs)
-    idf = get_idf(docs)
+    idf = get_idf(docs)[0]
     df_tf_idf = df_tf.copy()
 
     for word in words_set:
         for i in range(number_of_docs):
-            df_tf_idf[word][i] = df_tf[word][i] * idf[word]
+            value = df_tf.loc[i, [word]] * idf[word]
+            df_tf_idf.loc[i, [word]] = value
+
+    # df_tf_idf = df_tf_idf.T
 
     return df_tf_idf
-
